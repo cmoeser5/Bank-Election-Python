f43e38ea124ab306254a481de519e53b46f44102
cmoeser5 <carolineannmoeser@gmail.com>
2020-09-02 03:21:15 +0000

    add candidate votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,6 +2,10 @@ import os
 import csv
 
 total_votes = 0
+Khan_votes = 0
+Correy_votes = 0
+Li_votes = 0
+OTooley_votes = 0
 
 path = os.path.join("Resources", "election_data.csv")
 
@@ -11,6 +15,22 @@ with open(path, "r") as file:
     
     for row in csv_reader:
         total_votes += 1
+        
+        if row[2] == "Khan":
+            Khan_votes += 1
+            
+        elif row[2] == "Correy":
+            Correy_votes += 1
+            
+        elif row[2] == "Li":
+            Li_votes += 1
+            
+        else:
+            OTooley_votes += 1
+            
 
-print(total_votes)
+print(Khan_votes)
+print(Correy_votes)
+print(Li_votes)
+print(OTooley_votes)
         
