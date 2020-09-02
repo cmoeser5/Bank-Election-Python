a264c7ab42f6156709fa9daaa42a525df3f0442d
cmoeser5 <carolineannmoeser@gmail.com>
2020-09-02 03:45:11 +0000

    updated candidate votes and add vote percentage

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,10 +2,10 @@ import os
 import csv
 
 total_votes = 0
-Khan_votes = 0
-Correy_votes = 0
-Li_votes = 0
-OTooley_votes = 0
+Khan_votes = []
+Correy_votes = []
+Li_votes = []
+OTooley_votes = []
 
 path = os.path.join("Resources", "election_data.csv")
 
@@ -17,20 +17,32 @@ with open(path, "r") as file:
         total_votes += 1
         
         if row[2] == "Khan":
-            Khan_votes += 1
-            
-        elif row[2] == "Correy":
-            Correy_votes += 1
-            
-        elif row[2] == "Li":
-            Li_votes += 1
-            
-        else:
-            OTooley_votes += 1
-            
-
-print(Khan_votes)
-print(Correy_votes)
-print(Li_votes)
-print(OTooley_votes)
+            Khan_votes.append(row[2])
+        total_Khan_votes = len(Khan_votes)
         
+        if row[2] == "Correy":
+            Correy_votes.append(row[2])
+        total_Correy_votes = len(Correy_votes)
+        
+        if row[2] == "Li":
+            Li_votes.append(row[2])
+        total_Li_votes = len(Li_votes)
+        
+        if row[2] == "O'Tooley":
+            OTooley_votes.append(row[2])
+        total_OTooley_votes = len(OTooley_votes)
+
+print(total_Khan_votes)
+print(total_Correy_votes)
+print(total_Li_votes)
+print(total_OTooley_votes)
+
+Khan_percent = (total_Khan_votes / total_votes) * 100
+Correy_percent = (total_Correy_votes / total_votes) * 100
+Li_percent = (total_Li_votes / total_votes) * 100
+OTooley_percent = (total_OTooley_votes / total_votes) *100
+
+print(Khan_percent)
+print(Correy_percent)
+print(Li_percent)
+print(OTooley_percent)
