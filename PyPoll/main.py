3f64dd09ff1936a9b29d6fbbbfffa954a9f5932f
cmoeser5 <carolineannmoeser@gmail.com>
2020-09-02 04:44:52 +0000

    add winner

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,17 +9,18 @@ OTooley_votes = []
 
 path = os.path.join("Resources", "election_data.csv")
 
+
 with open(path, "r") as file:
     csv_reader = csv.reader(file)
     csv_header = next(csv_reader)
     
     for row in csv_reader:
         total_votes += 1
-        
+                                  
         if row[2] == "Khan":
             Khan_votes.append(row[2])
-        total_Khan_votes = len(Khan_votes)
-        
+        total_Khan_votes = len(Khan_votes) 
+                                  
         if row[2] == "Correy":
             Correy_votes.append(row[2])
         total_Correy_votes = len(Correy_votes)
@@ -31,18 +32,24 @@ with open(path, "r") as file:
         if row[2] == "O'Tooley":
             OTooley_votes.append(row[2])
         total_OTooley_votes = len(OTooley_votes)
-
-print(total_Khan_votes)
-print(total_Correy_votes)
-print(total_Li_votes)
-print(total_OTooley_votes)
+        
 
 Khan_percent = (total_Khan_votes / total_votes) * 100
 Correy_percent = (total_Correy_votes / total_votes) * 100
 Li_percent = (total_Li_votes / total_votes) * 100
 OTooley_percent = (total_OTooley_votes / total_votes) *100
 
-print(Khan_percent)
-print(Correy_percent)
-print(Li_percent)
-print(OTooley_percent)
+        
+dict_winner = {
+    "Khan": int(total_Khan_votes),
+    "Correy": int(total_Correy_votes),
+    "Li": int(total_Li_votes),
+    "O'Tooley": int(total_OTooley_votes)
+    }
+
+candidates = list(dict_winner.keys())
+votes = list(dict_winner.values())
+
+winner = candidates[votes.index(max(votes))]
+
+
