9f676b4737440ea7f465d67234e9f5e4204d7e38
cmoeser5 <carolineannmoeser@gmail.com>
2020-09-02 05:07:11 +0000

    add net total

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,6 +4,7 @@ import csv
 path = os.path.join("Resources", "budget_data.csv")
 
 total_months = 0
+profit_loss = []
 
 with open(path, "r") as file:
     csv_reader = csv.reader(file)
@@ -12,4 +13,10 @@ with open(path, "r") as file:
     for row in csv_reader:
         total_months += 1
         
+        profit_loss.append(int(row[1]))
+        
+        
+net_total = sum(profit_loss)  
+
 print(total_months)
+print(net_total)
