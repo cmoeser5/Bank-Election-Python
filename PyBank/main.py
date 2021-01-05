4ae61e6be8025c70b46957f4ac5460319e541d8b
cmoeser5 <carolineannmoeser@gmail.com>
2021-01-05 17:54:58 +0000

    add comments to code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,14 +1,17 @@
 import os 
 import csv
 
+#create a path to the csv
 path = os.path.join("Resources", "budget_data.csv")
 
 outfile = os.path.join("Analysis", "PyBank.txt")
 
+#generate empty lists
 total_months = []
 profit_loss = []
 monthly_change = []
 
+#read csvs and append data to the empty lists
 with open(path, "r") as file:
     csv_reader = csv.reader(file)
     csv_header = next(csv_reader)
@@ -21,19 +24,20 @@ with open(path, "r") as file:
     for i in range(1, len(profit_loss)):
         monthly_change.append(int(profit_loss[i]) - int(profit_loss[i - 1]))
     
-        
+#calculate the average change and net total        
 avg_change = sum(monthly_change) / len(monthly_change)       
 net_total = sum(profit_loss)  
 
-
+#calculate the greatest monthly increase and decrease
 greatest_increase = max(monthly_change)
 greatest_decrease = min(monthly_change)
 
+#find the date of the greatest monthly increase and decrease
 greatest_increase_date = monthly_change.index(max(monthly_change)) + 1
 greatest_decrease_date = monthly_change.index(min(monthly_change)) + 1 
 
 
-
+#generate output
 output = f"""
 Financial Analysis
 ----------------------------
